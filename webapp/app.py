c016862eb1c768102163dfbaf946cbdfbf89755e
Mustafa Arici <mustafa.arici@neu.edu.tr>
2013-05-21 05:49:20 +0300

    spawn a capturer proccess to replay when the pcap upload finished

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,5 @@
 import os
+from multiprocessing import Process
 from flask import Flask, render_template, request, session, redirect, url_for
 from werkzeug import secure_filename
 import uuid
@@ -15,6 +16,22 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+def spawn_capturer(field_name, pcap):
+    import sys
+    def parse_uri(uri):
+        'amp://192.168.1.1:2000'
+        decomposed = uri.replace('/', '').split(":")
+        return {'protocol':decomposed[0], 'host':decomposed[1], 'port':int(decomposed[2])}
+    path = os.path.join(os.path.dirname(os.path.join(os.path.realpath(__file__))), '..')
+    sys.path.append(path)
+    import ssl_capturer
+        
+    uri = parse_uri("amp://212.175.35.222:443")
+    service = ssl_capturer.CapturingService(field_name, None, None, uri['host'], uri['port'], mode='pcap', pcap=pcap)
+    service.ready()
+    print "Replaying Started"
+    service.run()
+    
 
 @app.route("/ssl-monitor/")
 def index():
@@ -33,7 +50,14 @@ def replay():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(str(session['uid']))
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            # saved
+                
+            p = Process(target=run_capturer, args=(filename, file_path))
+            p.start()
+            print "Proccess has been spawned!"
+
             return redirect(url_for('game',
                                     fieldname=filename))
             
@@ -48,7 +72,8 @@ def game():
             fieldname = request.args.get('fieldname')
             return render_template("game.html", fieldname)
 
-    return render_template("405.html", fieldname)
+    return render_template("405.html", fieldname)    
+
 
 if __name__ == "__main__":
     app.secret_key = 'bill gates is gay!'
