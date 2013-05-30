52b8f0890e8061a0a226b0eb98017a9b6983941f
Mustafa Arici <mustafa.arici@neu.edu.tr>
2013-05-30 17:01:24 +0300

    navbar fix ui fixes

--- webapp/forms.py
+++ webapp/forms.py
@@ -3,8 +3,8 @@ from wtforms import *
 class UserRegistrationForm(Form):
     name = TextField('Name Surname', [validators.Required(), validators.Length(min=3, max=128, message="Your name should be minimum 3, maximum 128 characters.")])
     email = TextField('E-Mail', [validators.Required(), validators.Email(message='Wrong E-Mail address.')])
-    team = TextField('RoboCup Team Name')
-    country = TextField('Country')
+    team = TextField('RoboCup Team Name', [validators.Required()])
+    country = TextField('Country', [validators.Required()])
     password = PasswordField('Password', [validators.Required(), validators.EqualTo('confirm',message='Password missmatch')])
     confirm = PasswordField('Repeat Password',[validators.Required()])
 
